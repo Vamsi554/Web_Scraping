76292995a54c06b7330cdb85430b529f36a1c8f7
Venkata Vamsi Krishna Bulusu <34600966+Vamsi554@users.noreply.github.com>
2019-09-01 00:08:00 +0530

    Create constants.py

---
+++ Insta_Scrapper/constants.py
@@ -0,0 +1,15 @@
+# Global Constants
+
+INSTAGRAM_URL = 'https://www.instagram.com'
+INSTAGRAM_QUERY_HASH_URL = 'https://www.instagram.com/graphql/query/?'
+BASE_URL = 'https://www.instagram.com/accounts/login/'
+LOGIN_URL = 'https://www.instagram.com/accounts/login/ajax/'
+USER_NAME = 'YOUR_USER_NAME_HERE'
+PASSWORD = 'YOUR_PASSWORD_HERE'
+USER_AGENT = 'Mozilla/5.0 (Windows NT 5.1; rv:41.0) Gecko/20100101 Firefox/41.0'
+USER_PROFILE_URL = 'https://www.instagram.com/@USERNAME/'
+
+# HTML TEMPLATE CODE 1
+HTML_TEMPLATE_1 = '<br><br><div class="row" id="design"><div class="col-md-4 col-md-offset-1"><div class="home-design-info"><figure><img class="img-circle img-responsive" src="XX_IMAGE_URL_XX"></figure></div></div><div class="col-md-6"><div><br><br><br><br><h4 style="text-align:center;"><b><i style="color: #e75480">XX_HEADING_BY_USER</i></b><br><br><i class="fa fa-clock-o" style="color:green;"></i><i> XX_UPLOAD_DTTM_XX</i><br><i class="fa fa-heart" style="color: #FF0000"></i><i> XX_LIKES_XX</i><br><i class="fa fa-comments-o" style="color:#add8e6;"></i><i> XX_COMMENTS_XX</i><br><b><i style="color:#D733FF"> XX_LOCATION_XX</i></b><br></h4></div></div></div>'
+HTML_TEMPLATE_2 = '<br><br><div class="row" id="design"><div class="col-md-3 col-md-offset-2"><div><br><br><br><br><h4 style="text-align:center;"><b><i style="color: #e75480"> XX_HEADING_BY_USER</i></b><br><br><i class="fa fa-clock-o" style="color:green;"></i><i> XX_UPLOAD_DTTM_XX</i><br><i class="fa fa-heart" style="color: #FF0000"></i><i> XX_LIKES_XX</i><br><i class="fa fa-comments-o" style="color:#add8e6;"></i><i> XX_COMMENTS_XX</i><br><b><i style="color:#D733FF"> XX_LOCATION_XX</i></b><br></h4></div></div><div class="col-md-7"><div class="home-design-info"><figure><img class="img-circle img-responsive" src="XX_IMAGE_URL_XX"></figure></div></div></div>'
+HTML_TEMPLATE_3 = '<br><br><div class="row" id="develop"><div class="col-md-4 col-md-push-9"><div class="home-dev-info"><figure><img class="img-circle img-responsive" src="XX_IMAGE_URL_XX"></figure></div></div><div class="col-md-8 col-md-pull-1"><div><br><br><br><br><h4 style="text-align:center;"><b><i style="color: #e75480"> XX_HEADING_BY_USER</i></b><br><br><i class="fa fa-clock-o" style="color:green;"></i><i> XX_UPLOAD_DTTM_XX</i><br><i class="fa fa-heart" style="color: #FF0000"></i><i> XX_LIKES_XX</i><br><i class="fa fa-comments-o" style="color:#add8e6;"></i><i> XX_COMMENTS_XX</i><br><b><i style="color:#D733FF"> XX_LOCATION_XX</i></b><br></h4></div></div></div>'
